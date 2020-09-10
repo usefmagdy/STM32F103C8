7fe3f27beb1a4be9f9425d8de7c3085419d9c4cf
Yousif magdy <64543334+usefmagdy@users.noreply.github.com>
2020-09-10 03:05:30 +0200

    Update NVIC_config.h

--- MCAL/NVIC/NVIC_config.h
+++ MCAL/NVIC/NVIC_config.h
@@ -1,1 +1,19 @@
+/********************************************************************************/
+/* Auther		: yousif magdy												  	*/
+/* Date			: 14 AUG 2020													*/
+/* Version		: V01	 														*/
+/* Description  : Function prototype											*/
+/********************************************************************************/
+#ifndef NVIC_CONFIG_H
+#define NVIC_CONFIG_H
+/*OPTIONS :	GROUP4_SUB0
+			GROUP3_SUB1
+			GROUP2_SUB2
+			GROUP1_SUB3
+			GROUP0_SUB4*/
 
+
+#define MNVIC_GROUP_SUB_DISTRIBUTION GROUP0_SUB4
+
+
+#endif
