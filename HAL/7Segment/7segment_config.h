cd8ee8219398f7749ff0f9fca8bfc3f2c3da5d0e
Yousif magdy <64543334+usefmagdy@users.noreply.github.com>
2020-09-10 03:36:15 +0200

    Create 7segment_config.h

---
+++ HAL/7Segment/7segment_config.h
@@ -0,0 +1,17 @@
+/********************************************************************************/
+/* Auther		: yousif magdy									                          			  	*/
+/* Date			: 14 AUG 2020													                              */
+/* Version		: V01	 														                                */
+/* Description  : User Configuration of 7 segment								                */
+/********************************************************************************/
+#ifndef _7SEGMENT_CONFIG_H
+#define _7SEGMENT_CONFIG_H
+
+/*OPTIONS :	OUTPUT_PORT_SPEED_2MHZ_PP
+			OUTPUT_PORT_SPEED_10MHZ_PP
+			OUTPUT_PORT_SPEED_50MHZ_PP*/
+#define Mode OUTPUT_PORT_SPEED_10MHZ_PP
+
+
+
+#endif
