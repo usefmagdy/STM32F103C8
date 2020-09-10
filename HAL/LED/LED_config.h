f22da54276dcdf0d7e37e6952693996ed9a944c3
Yousif magdy <64543334+usefmagdy@users.noreply.github.com>
2020-09-10 03:33:44 +0200

    Create LED_config.h

---
+++ HAL/LED/LED_config.h
@@ -0,0 +1,15 @@
+/********************************************************************************/
+/* Auther		: yousif magdy												                             	*/
+/* Date			: 13 August 2020												                            */
+/* Version		: V01	 														                                */
+/* Descripation	: LED Configuration 											                      */
+/********************************************************************************/
+#ifndef _LED_CONFIG_H
+#define _LED_CONFIG_H
+
+/*Options :	
+			*/
+#define FREQ_SPEED 
+
+
+#endif
