338a231f7a4129ca96b9e6a9fdd74d14d046060e
Yousif magdy <64543334+usefmagdy@users.noreply.github.com>
2020-09-10 02:58:28 +0200

    Update RCC_config.h

--- MCAL/RCC/RCC_config.h
+++ MCAL/RCC/RCC_config.h
@@ -1,1 +1,28 @@
+/****************************************************************************/
+/* Auther	: yousif magdy												  	*/
+/* Date		: 08 August 2020												*/
+/* Version	: V01	 														*/
+/****************************************************************************/
 
+
+#ifndef _RCC_CONFIG_H
+#define _RCC_CONFIG_H
+/*option 	RCC_HSE_CRYSTAL
+			RCC_HSE_RC
+			RCC_HSI
+			RCC_PLL	*/
+#define RCC_CLOCK_TYPE RCC_HSE_CRYSTAL
+
+/*OPTIONS:	RCC_PLL_IN_HSI_DIV2
+			RCC_PLL_IN_HSE_DIV2
+			RCC_PLL_IN_HSE	*/
+/*NOTE: YOU HAVE TO CHOOSE IF RCC IS PLL*/
+			
+#if RCC_CLOCK_TYPE == RCC_PLL
+#define RCC_PLL_INPUT RCC_PLL_IN_HSE_DIV2
+#endif
+/*OPTIONS: 2: 16*/
+/*NOTE: YOU HAVE TO CHOOSE IF RCC IS PLL*/
+#define RCC_PLL_MUL_VAL 4
+
+#endif
